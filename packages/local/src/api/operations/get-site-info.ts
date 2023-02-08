1ed3586d4e97c5f60ebe9b1a4712398bf5f4bd43
christinaprof <104025887+christinaprof@users.noreply.github.com>
2023-02-08 15:18:40 -0500

    Update get-site-info.ts

--- packages/local/src/api/operations/get-site-info.ts
+++ packages/local/src/api/operations/get-site-info.ts
@@ -23,19 +23,82 @@ export default function getSiteInfoOperation({}: OperationContext<any>) {
     return Promise.resolve({
       categories: [
         {
-          id: 'new-arrivals',
-          name: 'New Arrivals',
-          slug: 'new-arrivals',
-          path: '/new-arrivals',
+          id: 'accesories',
+          name: 'Accesories',
+          slug: 'accesories',
+          path: '/accesories',
         },
         {
-          id: 'featured',
-          name: 'Featured',
-          slug: 'featured',
-          path: '/featured',
+          id: 'dresses',
+          name: 'Dresses',
+          slug: 'dresses',
+          path: '/dresses',
+        },
+        {
+          id: 'jackets',
+          name: 'Jackets',
+          slug: 'jackets',
+          path: '/jackets',
+        },
+        {
+          id: 'pants',
+          name: 'Pants',
+          slug: 'pants',
+          path: '/pants',
+        },
+        {
+          id: 'shorts',
+          name: 'Shorts',
+          slug: 'shorts',
+          path: '/shorts',
+        },
+        {
+          id: 'skirts',
+          name: 'Skirts',
+          slug: 'skirts',
+          path: '/skirts',
+        },
+        
+        {
+          id: 'tops',
+          name: 'Tops',
+          slug: 'tops',
+          path: '/tops',
+        },
+        {
+          id: 'business',
+          name: 'Business',
+          slug: 'business',
+          path: '/business',
+        },
+        {
+          id: 'formal',
+          name: 'Formal',
+          slug: 'formal',
+          path: '/formal',
+        },
+        
+      ],
+      brands: [
+        {
+          id: 'forever21',
+          name: 'Forever21',
+          slug: 'forever21',
+          path: '/forever21',
+        },
+        {
+          id: 'nike',
+          name: 'Nike',
+          slug: 'nike',
+          path: '/nike',
+        },
+        {
+          id: 'shein',
+          name: 'Shein',
+          slug: 'shein',
+          path: '/shein',
         },
       ],
-      brands: [],
     })
   }
 
